4609c4a414f535a56073324840c8fb6a4e1eb943
SIDDHARTH KAUSHIK <siddharth9k9@gmail.com>
2019-11-08 09:00:40 -0800

    added

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/4aeda1a2642ec60d53292149b8d2c435/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/4aeda1a2642ec60d53292149b8d2c435/' + latitude + ',' + longitude+'?units=si'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
